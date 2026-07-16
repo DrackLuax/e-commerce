46a573cc62af38eb9a551c51f8943a159ab7e68e
Leonardo Picanço Queiroz Fontinele <leoplayjdk@gmail.com>
2026-07-16 19:51:01 -0300

    refactor(lista-produtos): adiciona lista de produtos com signal

--- leonardo_fontinele/e-commerce/src/app/features/produtos/lista-produtos/lista-produtos.ts
+++ leonardo_fontinele/e-commerce/src/app/features/produtos/lista-produtos/lista-produtos.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { Produto } from '../produto/produto';
+import { signal } from '@angular/core';
+
 @Component({
   selector: 'app-lista-produtos',
   imports: [Produto],
@@ -7,11 +9,11 @@ import { Produto } from '../produto/produto';
   styleUrl: './lista-produtos.css',
 })
 export class ListaProdutos {
-  produtos = [
+  produtos = signal([
     {nome:'RTX 5090', preco: 12999.99},
     {nome:'R5 5600G', preco: 599.99},
     {nome:'32GB RAM DDR4 3600HZ', preco: 949.99},
-  ];
+  ]);
   exibirProduto(nome: string){
     console.log('Produto Selecionado: ', nome);
   }
