df560733172ebe0f18d12e1bde89e85d98759c81
Leonardo Picanço Queiroz Fontinele <leoplayjdk@gmail.com>
2026-07-16 21:03:03 -0300

    feat(lista-produtos): adiciona computed totalProdutos para contar itens da lista

--- leonardo_fontinele/e-commerce/src/app/features/produtos/lista-produtos/lista-produtos.ts
+++ leonardo_fontinele/e-commerce/src/app/features/produtos/lista-produtos/lista-produtos.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { Produto } from '../produto/produto';
 import { signal } from '@angular/core';
+import { computed } from '@angular/core'
 
 @Component({
   selector: 'app-lista-produtos',
@@ -9,12 +10,23 @@ import { signal } from '@angular/core';
   styleUrl: './lista-produtos.css',
 })
 export class ListaProdutos {
+  //!Lista com dados - Array
   produtos = signal([
     {nome:'RTX 5090', preco: 12999.99},
     {nome:'R5 5600G', preco: 599.99},
     {nome:'32GB RAM DDR4 3600HZ', preco: 949.99},
   ]);
-  exibirProduto(nome: string){
+  //!Função para exibir produtos selecionados no console
+  exibirProduto (nome: string){
     console.log('Produto Selecionado: ', nome);
   }
+  //!Função que adiciona produto usando metodo update
+  adcionarProduto(){
+    this.produtos.update(listaAtual => [
+      ...listaAtual,
+      {nome:'Playstation 5', preco:3000},
+    ]);
+  }
+  //!Função que contabiliza a quantidade de produtos na lista
+  totalProdutos = computed(() => this.produtos().length); 
 }
