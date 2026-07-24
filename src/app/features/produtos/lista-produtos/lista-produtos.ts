afa959d154502aa36f144a24bda31276940b0d80
Luana Cardoso <luanamartinscardoso02@gmail.com>
2026-07-24 18:54:50 -0300

    feat: integra API de produtos e reorganiza componente ListaProdutos

--- src/app/features/produtos/lista-produtos/lista-produtos.ts
+++ src/app/features/produtos/lista-produtos/lista-produtos.ts
@@ -5,35 +5,76 @@ import { computed } from '@angular/core';
 import { PrecoFormatadoPipe } from '../../../shared/pipes/preco-formatado-pipe';
 import { effect } from '@angular/core';
 import { UpperCasePipe } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-lista-produtos',
-  imports: [Produto, PrecoFormatadoPipe, UpperCasePipe],
+  imports: [Produto, PrecoFormatadoPipe,UpperCasePipe],
   templateUrl: './lista-produtos.html',
   styleUrl: './lista-produtos.css',
 })
 export class ListaProdutos {
+  //! SIGNALS
+
   //!lista com dados - Array
-  produtos = signal([
-    { nome: 'Fone Gamer', preco: 345.99 },
-    { nome: 'Teclado Gamer Arceus', preco: 456.99 },
-    { nome: 'RTX 5090', preco: 25999.99 },
-  ]);
-  //!função para exibir produtos selecionados pelo usuário no console
-  exibirProduto(nome: string) {
-    console.log('Produto Selecionado: ', nome);
-    this.produtoSelecionado.set(nome);
-  }
-  //!funcão que adiciona produtos usando metodo update()
-  adicionarProduto() {
-    this.produtos.update((listaAtual) => [...listaAtual, { nome: 'Playstation 5', preco: 3000 }]);
-  }
+  produtos = signal<{ nome: string; preco: number }[]>([]);
+
+  carregando = signal(true);
+
+  //!metodo para criar um estado de seleção com signal string / null
+  produtoSelecionado = signal<string | null>(null);
+
+  //!metodo para criar um estado para carrinho com signal
+  carrinho = signal<{ nome: string; preco: number }[]>([]);
+
+  //! COMPUTED
+
   //!função que contabiliza a quantidade de produtos na lista com o metodo computed()
   totalProdutos = computed(() => this.produtos().length);
+
   //!funcao que calcula o valor total do produtos usando o metodo computed()
   valorTotal = computed(() => {
     return this.produtos().reduce((total, item) => total + item.preco, 0);
   });
+
+  //!metodo para calcular a quantidade total de itens no carrinho
+  quantidadeCarrinho = computed(() => this.carrinho().length);
+
+  //!metodo que calcula o valor total dos itens do carrinho
+  totalCarrinho = computed(() => {
+    return this.carrinho().reduce((total, item) => total + item.preco, 0);
+  });
+
+  //! METODO HTTP CLIENT (API)
+  carregarProdutos(){
+    this.carregando.set(true);
+    this.http.get<{title: string; price: number}[]>
+    ('https://fakestoreapi.com/products').subscribe({
+      next: (dados) => {
+        const produtosFormatados = dados.map(p =>({
+          nome: p.title,
+          preco: p.price,
+        }));
+        this.produtos.set(produtosFormatados);
+        this.carregando.set(false);
+      },
+      error: (erro) => {
+        console.error('Erro ao carregar produtos: ', erro);
+        this.carregando.set(false);
+      }
+    });
+  }
+
+  //! METODO UPDATE
+  //!funcão que adiciona produtos usando metodo update()
+  adicionarProduto() {
+    this.produtos.update((listaAtual) => [...listaAtual, { nome: 'Playstation 5', preco: 3000 }]);
+  }
+
+  adicionarAoCarrinho(produto: { nome: string; preco: number }) {
+    this.carrinho.update((listaAtual) => [...listaAtual, produto]);
+  }
+
   //!função para substituir a lista atual usando o metodo set()
   substituirProdutos() {
     this.produtos.set([
@@ -44,31 +85,35 @@ export class ListaProdutos {
       { nome: 'Headset', preco: 30 },
     ]);
   }
+
+  //! CONSTRUCTOR
+
   //!metodo para monitorar as alterações em tempo real usando effect()
-  constructor() {
+  //**Carrega a API**/
+  constructor(private http: HttpClient) {
+    //**Carrega a API**/
+    this.carregarProdutos();
+
+    //!effect continuam iguais - não mexer
     effect(() => {
       console.log('Lista de Produtos Alterados: ', this.produtos());
     });
+
     effect(() => {
       console.log('Valor Total Atualizado: ', this.valorTotal());
     });
+
     effect(() => {
       if (typeof document !== 'undefined') {
         document.title = `(${this.totalProdutos()}) - Sunrise Store`;
       }
     });
   }
-  //!metodo para criar um estado de seleção com signal string / null
-  produtoSelecionado = signal<string | null>(null);
-  //!metodo para criar um estado para carrinho com signal
-  carrinho = signal<{ nome: string; preco: number }[]>([]);
-  adicionarAoCarrinho(produto: { nome: string; preco: number }) {
-    this.carrinho.update((listaAtual) => [...listaAtual, produto]);
+
+  //! NÃO MEXER
+  //!função para exibir produtos selecionados pelo usuário no console
+  exibirProduto(nome: string) {
+    console.log('Produto Selecionado: ', nome);
+    this.produtoSelecionado.set(nome);
   }
-  //!metodo para calcular a quantidade total de itens no carrinho
-  quantidadeCarrinho = computed(() => this.carrinho().length);
-  //!metodo que calcula o valor total dos itens do carrinho
-  totalCarrinho = computed(() => {
-    return this.carrinho().reduce((total, item) => total + item.preco, 0);
-  });
 }
