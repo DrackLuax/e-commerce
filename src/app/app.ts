90a3e7a89da3935a0f794b1410ab25f64d8f0a7e
Leonardo Picanço Queiroz Fontinele <leoplayjdk@gmail.com>
2026-08-05 21:44:19 -0300

    feat: integra header e botões de autenticação

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,16 +2,18 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet, RouterLink } from '@angular/router';
 import { UpperCasePipe } from '@angular/common';
 import { usuarioLogado, login, logout } from './core/auth';
+import { MatButtonModule } from '@angular/material/button';
+import { Header } from './shared/layout/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, UpperCasePipe], //Stand Alone = Independente
+  imports: [RouterOutlet, RouterLink, UpperCasePipe, MatButtonModule, Header], //Stand Alone = Independente
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('e-commerce');
-  nomeLoja = 'Sunrise Store';
+  nomeLoja = 'Mercado Branco';
   usuarioLogado = usuarioLogado;
   login = login;
   logout = logout;
