633e2b4e83eb2377fc1ea6268d682b175af0f6fc
Leonardo Picanço Queiroz Fontinele <leoplayjdk@gmail.com>
2026-08-13 19:54:19 -0300

    refactor: remove lógica de autenticação temporária do componente raiz

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,20 +1,12 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet, RouterLink } from '@angular/router';
-import { UpperCasePipe } from '@angular/common';
-import { usuarioLogado, login, logout } from './core/auth';
-import { MatButtonModule } from '@angular/material/button';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
 import { Header } from './shared/layout/header/header';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, UpperCasePipe, MatButtonModule, Header], //Stand Alone = Independente
-  templateUrl: './app.html',
-  styleUrl: './app.css',
+selector: 'app-root',
+imports: [RouterOutlet, Header],
+templateUrl: './app.html',
+styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('e-commerce');
-  nomeLoja = 'Mercado Branco';
-  usuarioLogado = usuarioLogado;
-  login = login;
-  logout = logout;
-}
+export class App {}
