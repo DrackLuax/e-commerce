efeb4248f56a682131e913b94155f6829a58c4a4
Leonardo Picanço Queiroz Fontinele <leoplayjdk@gmail.com>
2026-08-19 19:29:38 -0300

    refactor: migra adminGuard para usar AuthFacade

--- src/app/core/admin.guard.ts
+++ src/app/core/admin.guard.ts
@@ -1,18 +1,15 @@
-import { inject } from "@angular/core";
-import { CanActivateFn, Router } from "@angular/router";
-import { AuthService } from "./services/auth.service";
-
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthFacade } from './facades/auth.facade';
 export const adminGuard: CanActivateFn = () => {
-    const router = inject(Router);
-    const authService = inject(AuthService);
-
-    if(!authService.usuarioAtual()){
-        return router.createUrlTree(["/login"]);
-    }
-    if(!authService.admin()){
-        return router.createUrlTree(['/acesso-negado']);
-    }
-
-    return true;
-
+const authFacade = inject(AuthFacade);
+const router = inject(Router);
+if (!authFacade.estaLogado()) {
+return router.createUrlTree(['/login']);
 }
+if (!authFacade.ehAdmin()) {
+return router.createUrlTree(['/acesso-negado']);
+}
+// Se estiver logado e for admin, libera o acesso.
+return true;
+};
